3ffe6dba351a122bdc7b01b25c89c89156cb53e4
Nina <ciz0@bk.ru>
2020-05-21 14:44:31 +0300

    Готово

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let widthcircle = CGFloat(128 / 2)
+        redColorView.layer.cornerRadius = widthcircle
+        yellowColorView.layer.cornerRadius = widthcircle
+        greenColorView.layer.cornerRadius = widthcircle
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        if startButton.titleLabel?.text == "Start" {
+            startButton.setTitle("Next", for: .normal)
+            redColorView.alpha = 1
+        } else {
+            if redColorView.alpha == 1 {
+                redColorView.alpha = 0.3
+                yellowColorView.alpha = 1
+            } else if yellowColorView.alpha == 1 {
+                yellowColorView.alpha = 0.3
+                greenColorView.alpha = 1
+            } else if greenColorView.alpha == 1 {
+                greenColorView.alpha = 0.3
+                redColorView.alpha = 1
+            }
+        }
+    }
 }
-
