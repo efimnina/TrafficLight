ac94d52fe40f92015b7f1d1a89a03bac3125b50d
Nina <ciz0@bk.ru>
2020-05-21 12:06:26 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Нина Ефимова on 21.05.2020.
+//  Copyright © 2020 Efim Nina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
